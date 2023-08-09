090eb386e03d6a5320617420c7b90af8d7495ec9
Lakshmi <lakshmikurmam2020@outlook.com>
2023-08-09 16:43:13 +0530

    table values pushed

--- retrieve_data.js
+++ retrieve_data.js
@@ -4,14 +4,14 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('data.db');
 
 // Query data from the 'sensor_data' table
-db.all('SELECT * FROM sensor_data', (err, rows) => {
+db.all('SELECT * FROM Message', (err, rows) => {
   if (err) {
     console.error('Error querying data from the database:', err.message);
   } else {
     // Print the retrieved data
     console.log('Retrieved data from the database:');
     rows.forEach((row) => {
-      console.log(`ID: ${row.id}, Device ID: ${row.device_id}, Value: ${row.value}, Timestamp: ${row.timestamp}`);
+      console.log(`ID: ${row.id}, Value: ${row.value}, Timestamp: ${row.timestamp}`);
     });
   }
 
